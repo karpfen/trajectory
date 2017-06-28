1546d8d3f070958a74d8cb2018878ac5db468022
Andreas Petutschnig <andreas@petutschnig.de>
2017-06-28 17:33:56 +0200

    rm postgres tests

--- tests/testthat/test-db_tools.R
+++ tests/testthat/test-db_tools.R
@@ -12,17 +12,3 @@ test_that ("read_sqlite", {
                                                     FALSE))
                testthat::expect_error (read_sqlite ("nonexistent", "", FALSE))
 })
-
-test_that ("postgres2sqlite", {
-               cred <- "../test_creds.csv"
-               tbl <- "error"
-               f_name <- "test.sqlite"
-               testthat::expect_error (postgres2sqlite (cred, tbl, f_name))
-})
-
-test_that ("postgres2gpkg", {
-               cred <- "../test_creds.csv"
-               tbl <- "error"
-               f_name <- "test.sqlite"
-               testthat::expect_error (postgres2gpkg (cred, tbl, f_name))
-})
