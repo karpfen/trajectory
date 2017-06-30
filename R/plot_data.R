671213bc19eea6e37c4d10ca406f506472cd9617
Andreas Petutschnig <andreas@petutschnig.de>
2017-06-30 11:16:07 +0200

    update plotting function

--- R/plot_data.R
+++ R/plot_data.R
@@ -3,22 +3,47 @@
 #'
 #' @param traj \code{sfc} containing trajectories.
 #'
+#' @param x The parameter displayed on the x axis.
+#'
+#' @param y The parameter displayed on the y axis.
+#'
+#' @param size The attribute represented by dot size or a fixed value.
+#'
 #' @export
-plot_ldm <- function (traj)
+plot_ldm <- function (traj, x, y, size)
 {
+    if (!x %in% names (traj))
+        stop_err (x)
+
+    if (!y %in% names (traj))
+        stop_err (y)
+
+    if (!size %in% names (traj))
+        stop_err (size)
+
     traj <- traj [traj$ldm != 0, ]
     traj$classes <-  with (traj,
-                              cut (traj$ldm, breaks =
-                                   quantile (traj$ldm, probs = seq (0, 1,
-                                                                    by = 0.25)),
+                              cut (traj [[x]], breaks =
+                                   quantile (traj [[x]],
+                                             probs = seq (0, 1, by = 0.25)),
                                    include.lowest = TRUE))
 
-    ggplot2::ggplot (data = traj, ggplot2::aes (y = traj$trajectory_length,
-                                                   x = traj$ldm,
-                                                   colour = traj$classes)) +
-        ggplot2::geom_point (ggplot2::aes (size = traj$number_of_points)) +
+    ggplot2::ggplot (data = traj, ggplot2::aes (x = traj [[x]], y = traj [[y]],
+                                                colour = traj$classes)) +
+        ggplot2::geom_point (ggplot2::aes (size = traj [[size]])) +
         ggplot2::coord_polar (start = -pi / 2, direction = -1) +
+        ggplot2::scale_x_continuous (limits = c (0, 360), breaks = 0:7 * 45,
+                                     name = x) +
+        ggplot2::scale_y_continuous (trans = "log", name = y) +
         ggplot2::theme_bw () +
-        ggplot2::scale_y_continuous (trans = "log") +
-        ggplot2::scale_x_continuous (limits = c (0, 360), breaks = 0:7 * 45)
+        ggplot2::theme (legend.position = "none")
+}
+
+#' Stops the program and generates an error message for the given attribute.
+#'
+#' @noRd
+stop_err <- function (attribute)
+{
+    stop (paste0 ("Attribute '", attribute,
+                  "' is not present in the trajectory data."))
 }
--- tests/testthat/test-plot_data.R
+++ tests/testthat/test-plot_data.R
@@ -4,6 +4,14 @@ test_that ("plot_ldm", {
                pts <- read_sqlite (f_name, tbl)
                traj <- make_trajectories (pts, "speed", "dist", 2)
                traj$ldm <- runif (dim (traj) [1])
-               pl <- plot_ldm (traj)
+               pl <- plot_ldm (traj, "ldm", "ldm", "ldm")
                testthat::expect_type (pl, "list")
+               e_msg <- "Attribute 'err' is not present in the trajectory data."
+               testthat::expect_error (plot_ldm (traj, "err", "ldm", "ldm"),
+                                       e_msg)
+               testthat::expect_error (plot_ldm (traj, "ldm", "err", "ldm"),
+                                       e_msg)
+               testthat::expect_error (plot_ldm (traj, "ldm", "ldm", "err"),
+                                       e_msg)
+
 })
